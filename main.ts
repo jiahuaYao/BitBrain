e81f46bf95722f2f3698cc8a8978520766c44756
jiahuaYao <101383807+jiahuaYao@users.noreply.github.com>
2024-10-03 17:47:05 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+/**
+* Cool LED functionalities
+*/
+namespace ledTricks {
+    /**
+    * Blinks the LED once.
+    */
+    //% block
+    export function blink() {
+        pins.LED.digitalWrite(true)
+        pause(500)
+        pins.LED.digitalWrite(false)
+        pause(500)
+    }
+}
